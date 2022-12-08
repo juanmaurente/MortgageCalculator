b6ca90652fcd4fddf0d856c9273cc4b0497e9e46
Juan Maurente <juanmaurente@gmail.com>
2022-12-09 00:00:19 +1100

    adding schedule mortgage section

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,38 +4,79 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int principal = (int) readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) readNumber("Period (Years): ", 1, 30);
 
-        int principal = (int) readNumber("Principal: ",1000,1_000_000);
-        float annualInterest = (float) readNumber("Annual Interest Rate (0% - 30%): ", 0, 30);
-        byte years = (byte) readNumber("Period (years): ", 0,30);
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
 
-        double mortgage = calculateMortgage(principal,annualInterest,years);
+    private static void printMortgage(int principal, float annualInterest, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterest, years);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-
-        System.out.println("\n\nMortgage: " + mortgageFormatted);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
     }
 
-    public static double calculateMortgage(int principal, float annualInterest, byte years){
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-        float  monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        short  numberOfPayments = (short) (years * MONTHS_IN_YEAR);
-
-        return  (principal * ( monthlyInterest * Math.pow( (1 + monthlyInterest) , (numberOfPayments)) / (Math.pow( (1 + monthlyInterest) , (numberOfPayments)) - 1) ));
+    private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
     }
-    public static double readNumber(String prompt, double min, double max){
+
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years) {
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+
+    public static double calculateBalance(
+            int principal,
+            float annualInterest,
+            byte years,
+            short numberOfPaymentsMade
+    ) {
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+    }
+    public static double readNumber(String prompt, double min, double max) {
         Scanner scanner = new Scanner(System.in);
         double value;
-        while(true) {
+        while (true) {
             System.out.print(prompt);
             value = scanner.nextFloat();
-            if (value <= max && value > min)
+            if (value >= min && value <= max)
                 break;
-            System.out.println("Please enter a number between " + min +" and " + max);
+            System.out.println("Enter a value between " + min + " and " + max);
         }
         return value;
     }
+
 
 }
