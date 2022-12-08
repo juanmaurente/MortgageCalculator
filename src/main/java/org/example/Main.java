c256f3e5c4a7629d93e7ce88ffaa4c0ca6079b87
Juan Maurente <juanmaurente@gmail.com>
2022-12-08 16:47:10 +1100

    Introducing some error handling to the inputs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.sun.source.tree.WhileLoopTree;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -13,25 +15,55 @@ public class Main {
         //https://www.wikihow.com/Calculate-Mortgage-Payments
         // mortgage = principal ((interest * (1+interest)exp years) / (1+interest)exp years -1)
 
-        int principal;
-        double mortgage;
-        double interestRate;
-        byte years;
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        int principal = 0;
+        float mortgage;
+        float annualInterest = 0;
+        float monthlyInterest = 0;
+        byte years = 0;
+        int numberOfPayments = 0;
+
         Scanner scanner = new Scanner(System.in);
         NumberFormat currency;
 
-        System.out.print("Principal: " );
-        principal = scanner.nextInt();
-        System.out.print("Interest Rate (%): ");
-        interestRate = ( scanner.nextDouble() ) / 12 / 100 ;
-        System.out.print("Years: ");
-        years = scanner.nextByte();
+        while(true) {
+            System.out.print("Principal ($1k - $1M): ");
+            principal = scanner.nextInt();
+            if (principal <= 1_000_000 && principal >= 1_000) {
+                break;
+            }
+            System.out.println("Please enter a number between 1,000 and 1,000,000");
 
-        double exp1 = Math.pow( (1 + interestRate) , (years * 12));
-        mortgage = principal * ( interestRate*exp1 / (exp1 - 1) );
+        }
+
+        while (true) {
+            System.out.print("Interest Rate (0% - 30%): ");
+
+            annualInterest = scanner.nextFloat();
+
+            if (annualInterest > 30 || annualInterest <= 0){
+                System.out.println("Please enter a number between 0 and 30");
+                continue;
+            }
+            monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            break;
+        }
+
+
+        while (true) {
+            System.out.print("Years (1 - 30): ");
+            years = scanner.nextByte();
+
+            if (years >= 1 && years <= 30) break;
+            else System.out.println("Please enter a number between 0 and 30");
+        }
+        numberOfPayments = years * MONTHS_IN_YEAR;
+
+        mortgage = (float) (principal * ( monthlyInterest * Math.pow( (1 + monthlyInterest) , (numberOfPayments)) / (Math.pow( (1 + monthlyInterest) , (numberOfPayments)) - 1) ));
 
         currency = NumberFormat.getCurrencyInstance();
-
         System.out.println("\n\nMortgage: " + currency.format(mortgage));
 
     }
